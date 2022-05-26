e113b3b576b0dfee2b0e09c894d217bf159146e0
youngduck <dudejr5839@naver.com>
2022-05-26 23:57:37 +0900

    [DOCS] #1 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+
+import React from "react";
+import Header from "./header"
+
+function App() {
+  
+  return (
+    <>
+    <div>안녕
+    </div>
+    <Header></Header>
+    </>
+    
+   
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = () =>{
+    
+    return(
+        <nav class = "nav_wrap">
+            <div class = "nav_header">
+                <div class = "nav_logo">
+
+                </div>
+                <div class = "nav_translate_btn">
+
+                </div>
+            </div>
+            <div class = "nav_menubar">
+                <ul calss = "nav_menu">
+                    <li><a href = "#">About OS</a></li>
+                    <li><a href = "#">Brand</a></li>
+                    <li><a href = "#">Project</a></li>
+                    <li><a href = "#">Press</a></li>
+                    <li><a href = "#">Contact</a></li>
+                </ul>
+                <div calss = "nav_signup">
+                    <form>
+                        <input type="text"></input>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
